582d382e8507bba487c6053a1c21549c12a2e7be
krishmandadi <mandadik3892@my.sduhsd.net>
2024-11-19 15:24:29 -0800

    sendControl

--- codrone-edu-python-2.0/codrone-edu-python-2.0/examples/choreography.py
+++ codrone-edu-python-2.0/codrone-edu-python-2.0/examples/choreography.py
@@ -9,17 +9,23 @@ class Choreography():
     def runSequence(self,swarm):
 
         drones = swarm.get_drone_objects()
+
         drone1 = drones[0]
         drone2 = drones[1]
         drone3 = drones[2]
         drone4 = drones[3]
 
-        drone1.send_absolute_position(-0.5, 0, 0, 0.5, 0, 0)
-        drone2.send_absolute_position(0, 0.5, 0, 0.5, 0, 0)
-        drone3.send_absolute_position(0.5, 0, 0, 0.5, 0, 0)
-        drone4.send_absolute_position(0, -0.5, 0, 0.5, 0, 0)
+        drone1.sendControlPosition(-0.5, 0, 0, 0.5, 0, 0)
+        drone2.sendControlPosition(0, 0.5, 0, 0.5, 0, 0)
+        drone3.sendControlPosition(0.5, 0, 0, 0.5, 0, 0)
+        drone4.sendControlPosition(0, -0.5, 0, 0.5, 0, 0)
 
         sleep(2.5)
 
-        swarm.all_drones("land")
+        drone1.sendControlPosition(0, -0.5, 0, 0.5, 0, 0)
+        drone2.sendControlPosition(-0.5, 0, 0, 0.5, 0, 0)
+        drone3.sendControlPosition(0, 0.5, 0, 0.5, 0, 0)
+        drone4.sendControlPosition(0.5, 0, 0, 0.5, 0, 0)
+
+        sleep(2.5)
 

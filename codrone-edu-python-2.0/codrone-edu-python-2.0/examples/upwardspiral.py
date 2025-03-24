849888780a0a5292999a58bbdd9ccfe6a9604efb
sarahsarahw <152912894+sarahsarahw@users.noreply.github.com>
2025-03-24 14:02:22 -0700

    w/ updated Swarm and drone indices

--- codrone-edu-python-2.0/codrone-edu-python-2.0/examples/upwardspiral.py
+++ codrone-edu-python-2.0/codrone-edu-python-2.0/examples/upwardspiral.py
@@ -1,23 +1,20 @@
 import math
 
-from swarm2 import *
-from codrone_edu import *
+from codrone_edu.swarm import *
 
 class Choreography():
 
     def __init__(self):
         return
 
-    def runSequence(self, swarm):
-        drones = swarm.get_drone_objects()
-        drone1, drone2, drone3 = drones[0], drones[1], drones[2]
-
+    def runSequence(self, swarm, selected_drone_indices):
         # Define spiral parameters
-        radius = 0.5  # radius of circle in meters
-        height = 0.5  # total height to ascend in meters
+        radius = 0.1  # radius of circle in meters
+        height = 0.2  # total height to ascend in meters
         duration = 4  # time to complete spiral in seconds
         steps = 20  # number of points in the spiral
 
+        sync = Sync()
         for i in range(steps):
             # Calculate position for this step
             t = i / (steps - 1)  # normalized time from 0 to 1
@@ -28,12 +25,12 @@ class Choreography():
             z = height * t  # Linear increase in height
 
             # Send position commands to all drones
-            for drone in [drone1, drone2, drone3]:
-                drone.sendControlPosition(x, y, z, 0.5, 0, 0)
+            for i in selected_drone_indices:
+                seq = Sequence(i)
+                seq.add('sendControlPosition', x, y, z, 0.5, 0, 0)
+                # Wait a short time between movements for smooth motion
+                seq.add('sequence_sleep', duration/steps)
+                sync.add(seq)
 
-            # Wait a short time between movements for smooth motion
-            sleep(duration / steps)
+        swarm.run(sync)
 
-
-
-        sleep(2)
